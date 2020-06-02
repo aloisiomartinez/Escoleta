e5d3c1841323dbad2de3399658946ac17e89a9ab
aloisiomartinez <aloisio.jacobs@hotmail.com>
2020-06-02 11:11:16 -0300

    Eslint Fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
 
-const users = ['Aloisio', 'Érica', 'Ana'];
-
-app.get('/users', (request, response) => {
-  return response.json(users);
-});
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-  const user = {
-    name: 'Diego',
-    email: 'diego@rocketseat.com.br',
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
