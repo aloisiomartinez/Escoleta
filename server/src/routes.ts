7389de4239494afbac15ca7674ec6bfe7d0fa0c0
aloisiomartinez <aloisiojacobs36@gmail.com>
2020-06-02 15:43:21 -0300

    Api Done

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import express from 'express';
 
-const routes = express.Router()
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-routes.get("/", (request, response) => {
-  console.log("Listagem de usuários");
-  response.json({ message: 'al'});
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
 
 export default routes;
