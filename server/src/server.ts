5fbf425d2ca2c1d10c8ec669a18f15b95381504c
aloisiomartinez <aloisio.jacobs@hotmail.com>
2020-06-02 10:26:21 -0300

    Eslint Added

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,27 @@ import express from 'express';
 
 const app = express();
 
+const users = ['Aloisio', 'Érica', 'Ana'];
+
 app.get('/users', (request, response) => {
-  response.json([
-    'Aloisio',
-    'Érica',
-    'Ana'
-  ]);
+  return response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego',
+    email: 'diego@rocketseat.com.br',
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
